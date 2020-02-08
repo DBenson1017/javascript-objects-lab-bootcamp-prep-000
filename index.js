ae1074a382b0124caa3657970ce2f1d1b9ddb6b9
Devin Benson <devinbenson@gmail.com>
2020-02-08 00:23:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var recipes = {}
+var object = {}
 
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, object, {[key]: value})
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  return 
+  var newObject = 
   
 }
